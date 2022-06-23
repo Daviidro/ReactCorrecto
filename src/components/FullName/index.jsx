89451ee5a12d14fb620681c645930136ec905baa
David <arq.drs23@gmail.com>
2022-06-22 22:36:11 -0500

    Rutas ejercicio

--- src/components/FullName/index.jsx
+++
@@ -1,7 +0,0 @@
-
-import "./Title.css"
-
-export default function fullName ({firstName, lastName})  {
-    return (<p>{firstName}{lastName}</p>
-    )
-}
--- src/components/Square/index.js
+++
@@ -1,5 +0,0 @@
-import "./Title.css"
-
-export default function switchColor (onOff)  {
-    return (<div className="square">ON-OFF</div>)
-}
